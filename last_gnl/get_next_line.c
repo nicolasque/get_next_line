0f795b7501da7bc76c1af8577fd18461b599df48
nicolasque <nicolasque1@gmail.com>
2024-01-22 21:05:06 +0100

    Fix get_next_line function

--- last_gnl/get_next_line.c
+++ last_gnl/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: nquecedo <nquecedo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/12 19:19:24 by nquecedo          #+#    #+#             */
-/*   Updated: 2024/01/22 20:43:49 by nquecedo         ###   ########.fr       */
+/*   Updated: 2024/01/22 21:04:55 by nquecedo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,21 +25,35 @@ char	*ft_read_line(int fd, char *buffer, char *readed)
 			break ;
 		bites_read = read(fd, buffer, BUFFER_SIZE);
 	}
-	printf("%s", readed);
 	return (readed);
+}
+
+char	*ft_proces_line(char *readed)
+{
+	char	*exit;
+	size_t 	line_size;
+
+	line_size = ft_strchr(readed, '\n') - readed;
+	exit = (char *)malloc(line_size + 2);
+	ft_memcpy(exit, readed, line_size + 1);
+	exit[line_size + 1] = '\0';
+	readed = ft_strchr(readed, '\n');
+	return (exit);
 }
 
 char	*get_next_line(int	fd)
 {
 	static char	*readed;
+	char		*exit;
 	char		buffer[BUFFER_SIZE + 1];
 
 	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, buffer, 0) < 0)
 		return (NULL);
-
-	readed = ft_read_line(fd, buffer, readed);
-
-	return (NULL);
+	if (!ft_strchr(readed, '\n'))
+		readed = ft_read_line(fd, buffer, readed);
+	exit = ft_proces_line(readed);
+	readed = ft_strchr(readed, '\n');
+	return (exit);
 }
 
 
@@ -52,10 +66,12 @@ int main()
 		printf("Error al leer el archivo");
 		return (-1);
 	}
-	get_next_line(fd);
+	// get_next_line(fd);
+	printf("\nEl tamaño del buffer que etsoy usando es de: %i\n", BUFFER_SIZE);
+	printf("llamada:\n%s__FIN\n\n", get_next_line(fd));
+	printf("\n============================================\n");
+	printf("llamada:\n%s__FIN\n\n", get_next_line(fd));
+	printf("\n============================================\n");
 	close(fd);
-// 	printf("\nEl tamaño del buffer que etsoy usando es de: %i\n", BUFFER_SIZE);
-// 	printf("llamada:\n%s__FIN\n\n", get_next_line(fd));
-// 	printf("\n============================================\n");
 }
 
